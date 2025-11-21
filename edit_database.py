ed46c1d13f83ebd08b8eda73aa6de47b3d849e95
R0KI1717 <danielrokicki3@gmail.com>
2025-11-21 18:34:50 +0100

    Add files via upload

---
+++ edit_database.py
@@ -0,0 +1,14 @@
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+connection = sqlite3.connect("sqlite.db", check_same_thread=False)
+cursor = connection.cursor()
+
+
+cursor.execute('INSERT INTO user VALUES (?, ?, ?)',
+               (1,'Rocket', generate_password_hash('qwerty123')))
+
+cursor.execute('UPDATE post SET author_id = 1;')
+
+connection.commit()
+connection.close()
